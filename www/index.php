bbcd3da684e319bff9709e982fc43af870dac496
michele25-10 <michele.gabri01@gmail.com>
2023-12-08 14:37:31 +0100

    Aggiornamento dinamico terminato

--- www/index.php
+++ www/index.php
@@ -34,8 +34,10 @@
         }
       });
     }
-
-    setInterval(aggiornaContenuto(), 500);
+    $(window).on('load', function() {
+      aggiornaContenuto()
+      setInterval(() => aggiornaContenuto(), 30000);
+    });
   </script>
 
   <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
